e920409b3df67e0626b62ef227c36ed86bd0825e
Skyller <smbaughman08@outlook.com>
2026-05-05 14:26:17 -0400

    Added a lot

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getImages()
         {
             //cell
             //const ore = choice(ores);
-            const ore = ores[cellNum];
+            const ore = ores[cellNum + 4];
             const oreEle = document.createElement("div");
             oreEle.className = "cell";
 
@@ -21,16 +21,57 @@ function getImages()
             //cell's / image's name
             const name = document.createElement("h3");
             name.textContent = ore;
+            //name.style.
             name.style.margin = "0px";
             name.style.marginTop = "2px";
             oreEle.appendChild(name);
             
+            oreEle.onclick = function() {clicked(name, oreImg)};
             document.getElementById(`row-${rowNum}`).appendChild(oreEle);
         }
     }
 }
-//last left off improving the visuals
-//next: probaly work on the onclick function
+
+function clicked(name, img)
+{
+  img.style.opacity = "1";
+  name.style.color = "black";
+
+  const won = document.getElementById("winnings");
+  if(name.textContent == "quartz")
+  {
+    won.textContent = `Ticket Winnings $${Number(won.textContent.split("$")[1]) + 5}`;
+  }
+  else if(name.textContent == "bauxite")
+  {
+    won.textContent = `Ticket Winnings $${Number(won.textContent.split("$")[1]) + 10}`;
+  }
+  else if(name.textContent == "uranium")
+  {
+    won.textContent = `Ticket Winnings $${Number(won.textContent.split("$")[1]) + 15}`;
+  }
+}
+
+function makeNew()
+{
+  if(sessionStorage.getItem("total"))
+  {
+    const newTotal = Number(sessionStorage.getItem("total")) + Number(document.getElementById("winnings").textContent.split("$")[1]);
+    sessionStorage.setItem("total", newTotal);
+  }
+  else
+  {
+    sessionStorage.setItem("total", "0");
+  }
+  window.location.reload();
+}
+
+function setTotal()
+{
+  document.getElementById("total").textContent = `$${sessionStorage.getItem("total")}`;
+}
+
+//last left off setting the total to save
 
 //these 2 are a helper functions
 function randint(min, max)
@@ -48,5 +89,6 @@ Notes:
 consider weighted changes
 find cool google font
 maybe add more cells
-//see if pages was deleted
+find some way to make the cell fade in as it's clicked
+make consistent cell sizes
 */
